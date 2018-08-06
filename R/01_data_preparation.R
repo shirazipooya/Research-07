ca4a102d836b64ffe7f167cef4919fc7fa3ad141
Pooya Shirazi <p.shirazi.a@gmail.com>
2018-08-06 14:13:55 +0000

    cv

--- R/01_data_preparation.R
+++ R/01_data_preparation.R
@@ -18,45 +18,50 @@ source(file = "./R/_find_meteorological_sites.R")
 source(file = "./R/_daily_met_data_cleansing.R")
 source(file = "./R/_find_air_quality_sites.R")
 
+# United States - California - Los Angeles
 lat = +034.0522
 lon = -118.2437
 
+# my met stations:
+stations <- c("722950-23174", "722956-03167", "722874-93134", "722955-03174", "722970-23129", "722976-03166")
+
 # Step 01: Meteorology Data -----------------------------------------------------------------------------
 
-# 01-01 find ISD site code: United States - Arizona - Phoenix
-phoenix_met_site <- find_meteorological_sites(lat = lat,
-                                              lon = lon,
-                                              n = 20,
-                                              end.year = "current",
-                                              plot = FALSE,
-                                              returnMap = FALSE)
+# 01-01 find ISD site code:
+find_met_site <- find_meteorological_sites(lat = lat,
+                                           lon = lon,
+                                           n = 2,
+                                           end.year = "current",
+                                           plot = FALSE,
+                                           returnMap = FALSE)
 
-phoenix_met_map <- find_meteorological_sites(lat = lat,
-                                             lon = lon,
-                                             n = 20,
-                                             end.year = "current",
-                                             plot = TRUE,
-                                             returnMap = TRUE)
+find_met_map <- find_meteorological_sites(lat = lat,
+                                          lon = lon,
+                                          n = 2,
+                                          end.year = "current",
+                                          plot = TRUE,
+                                          returnMap = TRUE)
 
 # 01-02 Import Meteorology Data:
-phoenix_met_data <- GSODR::get_GSOD(years = 2010:2017,
-                                    station = phoenix_met_site$CODE)
+download_met_data <- GSODR::get_GSOD(years = 2010:2017,
+                                     # station = find_met_site$CODE,
+                                     station = stations)
 
 # 01-03 Meteorology Data Cleansing:
-phoenix_met_data <- phoenix_met_data %>%
+download_met_data <- download_met_data %>%
         select(YEARMODA, STNID, LAT, LON, ELEV_M, TEMP, DEWP, SLP,
                STP, VISIB, WDSP, MXSPD, MAX, MIN, PRCP, EA, ES, RH)
 
-colnames(phoenix_met_data) <- c("date", "site", "lat", "lon", "elev", "tmean", "dp", "slp", "stp",
-                                "visib", "ws", "ws_max", "tmax", "tmin", "prec", "ea", "es", "rh")
+colnames(download_met_data) <- c("date", "site", "lat", "lon", "elev", "tmean", "dp", "slp", "stp",
+                                 "visib", "ws", "ws_max", "tmax", "tmin", "prec", "ea", "es", "rh")
 
-phoenix_sites <- unique(phoenix_met_data$site)
+met_sites <- unique(download_met_data$site)
 
-phoenix_data <- list()
+met_data <- list()
 
-for (i in phoenix_sites)
+for (i in met_sites)
 {
-        data <- phoenix_met_data %>%
+        data <- download_met_data %>%
                 filter(site == i)
         
         data <- daily_met_data_cleansing(data = data,
@@ -71,40 +76,40 @@ for (i in phoenix_sites)
         
         data$elev <- max(data$elev, na.rm = TRUE)
         
-        phoenix_data[[i]] <- data
+        met_data[[i]] <- data
 }
 
-phoenix_data <- do.call(what = rbind, 
-                        args = phoenix_data)
+met_data <- do.call(what = rbind, 
+                    args = met_data)
 
-rownames(phoenix_data) <- NULL
+rownames(met_data) <- NULL
 
 # 01-04 Save phoenix_data:
-phoenix_data$site <- as.factor(phoenix_data$site)
+met_data$site <- as.factor(met_data$site)
 
-for (i in phoenix_sites)
+for (i in met_sites)
 {
         print(i)
-        print(phoenix_data %>%
+        print(met_data %>%
                       filter(site == i) %>%
                       summarise_all(funs(sum(is.na(.)))))
 }
 
-saveRDS(object = phoenix_data,
-        file = "./data/met/phoenix_data.RDS")
+saveRDS(object = met_data,
+        file = "./data/met/met_data.RDS")
 
 # Step 02: Air Quality Data -----------------------------------------------------------------------------
 
 # 01-02 find air quality site code: United States - Arizona - Phoenix
 # c(81102,88101,42401,42602,44201,42101)
-phoenix_air_quality_site <- find_air_quality_sites(lat = lat,
-                                                   lon = lon,
-                                                   n = 20,
-                                                   state = NA,
-                                                   end.year = 2017:2018,
-                                                   parameter = c(81102,42401,42602,44201,42101),
-                                                   num_para = 5,
-                                                   plot = TRUE,
-                                                   returnMap = FALSE,
-                                                   map = phoenix_met_map)
+find_airquality_site <- find_air_quality_sites(lat = lat,
+                                               lon = lon,
+                                               n = 5,
+                                               state = NA,
+                                               end.year = 2017:2018,
+                                               parameter = c(81102,42401,42602,44201,42101),
+                                               num_para = 5,
+                                               plot = TRUE,
+                                               returnMap = FALSE,
+                                               map = find_met_map)
 
